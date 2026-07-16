d0ae722a43e2b6d15392bb17242e051efb0479c9
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2026-07-16 09:44:44 +0530

    ddg

--- lib/stopwatch/stopwatch.dart
+++ lib/stopwatch/stopwatch.dart
@@ -8,9 +8,47 @@ class StopWatchExample extends StatefulWidget {
 }
 
 class _StopWatchExampleState extends State<StopWatchExample> {
-  int seconds = 0;
+  // int seconds = 0;
+  int milliseconds = 0;
+  final laps = <int>[];
   late Timer timer;
   bool isRunning = false;
+
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+    print(laps);
+  }
+
+  void _clear() {
+    setState(() {
+      laps.clear();
+      milliseconds = 0;
+    });
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+        color: Theme.of(context).primaryColor,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Lap ${laps.length + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .copyWith(color: Colors.white),
+          ),
+          Text(
+            _milisToSecond(milliseconds),
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(color: Colors.white),
+          )
+        ]));
+  }
 
   @override
   void initState() {
@@ -25,17 +63,24 @@ class _StopWatchExampleState extends State<StopWatchExample> {
   void _onTick(Timer timer) {
     setState(() {
       if (isRunning) {
-        seconds++;
+        // seconds++;
+        milliseconds += 100;
       }
     });
   }
 
   void _startTimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+    timer = Timer.periodic(const Duration(milliseconds: 100), _onTick);
     setState(() {
-      seconds = 0;
+      // seconds = 0;
+      milliseconds = 0;
       isRunning = true;
     });
+  }
+
+  String _milisToSecond(milis) {
+    final seconds = milis / 1000;
+    return '$seconds seconds';
   }
 
   void _stopTimer() {
@@ -45,19 +90,6 @@ class _StopWatchExampleState extends State<StopWatchExample> {
     });
   }
 
-  void _pauseTimer() {
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void _resumeTimer() {
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  String _secondToText() => seconds == 1 ? '1 second' : '$seconds seconds';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,7 +101,7 @@ class _StopWatchExampleState extends State<StopWatchExample> {
           children: [
             Center(
               child: Text(
-                _secondToText(),
+                _milisToSecond(milliseconds),
                 style: const TextStyle(fontSize: 30),
               ),
             ),
@@ -86,24 +118,24 @@ class _StopWatchExampleState extends State<StopWatchExample> {
                 child: const Text('Start'),
               ),
               ElevatedButton(
-                onPressed: isRunning ? _pauseTimer : null,
+                onPressed: _lap,
                 style: ButtonStyle(
                   backgroundColor:
                       MaterialStateProperty.all<Color>(Colors.orange),
                   foregroundColor:
                       MaterialStateProperty.all<Color>(Colors.white),
                 ),
-                child: const Text('Pause'),
+                child: const Text('Lap'),
               ),
               ElevatedButton(
-                onPressed: isRunning ? null : _resumeTimer,
+                onPressed: _clear,
                 style: ButtonStyle(
                   backgroundColor:
                       MaterialStateProperty.all<Color>(Colors.blue),
                   foregroundColor:
                       MaterialStateProperty.all<Color>(Colors.white),
                 ),
-                child: const Text('Resume'),
+                child: const Text('Clear'),
               ),
               ElevatedButton(
                 onPressed: isRunning ? _stopTimer : null,
